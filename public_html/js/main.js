ebb53f9291bd5efa870cb1ee0d88348682378f54
Luka <luka.rako993@gmail.com>
2024-01-24 21:35:19 +0100

    error page and site done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -34,6 +34,57 @@ $(document).ready(function () {
     animation();
 
 
+    if($('.contact-form'.length > 0)){
+        
+        $('.contact-form').validate({
+           
+            highlight: function(element){
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function(element){
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                subject: {
+                    required: true,
+                    minlength: 5
+                },
+                message: {
+                    required: true,
+                    minlength: 10
+                }               
+            },
+            messages : {
+                name : {
+                    required: 'The Name* field is required!',
+                    minlength: 'Minimum length is 3 characters!'
+                },
+                email : {
+                    required: 'The Email* field is required!',
+                    email: 'Please enter an valid email adress!'
+                },
+                subject : {
+                    required: 'The Subject* field is required!'
+                },
+                message : {
+                    required: 'The Message* field is required!'
+                }
+            },
+            errorElement : 'p',
+            errorPlacement : function(error, element){
+                error.appendTo(element.closest('.form-group').find('.error-msg'));
+            }
+        });
+        
+    }
 
 
 });
